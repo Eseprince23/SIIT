c53f9b31065da1b677f9022b3e61118b981287f6
Eseprince23 <117567299+Eseprince23@users.noreply.github.com>
2022-11-06 09:43:08 +0000

    Add files via upload

---
+++ storage/framework/views/fcefd9ec46a22a6634b020e773a9222913fc3811.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <title>Document</title>
+</head>
+<body>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-4 col-md-offset-4">
+                <h4>Registration</h4>
+                            <form action="<?php echo e(route('register.custom')); ?>" method="POST">
+                                <?php echo csrf_field(); ?>
+                                <div class="form-group">
+                                    <label for="Full Name">Full Name</label>
+                                    <input type="text" class="form-control" placeholder="Enter Full Name" name="name">
+                                    <?php if($errors->has('name')): ?>
+                                    <span class="text-danger"><?php echo e($errors->first('name')); ?></span>
+                                    <?php endif; ?>
+                                </div>
+                                <div class="form-group">
+                                    <label for="Email">Email</label>
+                                    <input type="text" class="form-control" placeholder="Enter Email" name="email">
+                                    <?php if($errors->has('email')): ?>
+                                    <span class="text-danger"><?php echo e($errors->first('email')); ?></span>
+                                    <?php endif; ?>
+                                </div>
+                                <div class="form-group">
+                                    <label for="Passwor">Password</label>
+                                    <input type="password" class="form-control" placeholder="Enter Password" name="password">
+                                    <?php if($errors->has('password')): ?>
+                                    <span class="text-danger"><?php echo e($errors->first('password')); ?></span>
+                                    <?php endif; ?>
+                                </div>
+                                <div class="form-group">
+                                    <button class="btn btn-block btn-primary" type="submit">Register</button>
+
+                                </div><br>
+                                <a href="login">Registered user Login here</a>
+                            
+                             </form>
+            </div> 
+        </div>
+    </div>
+</body>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+</html><?php /**PATH C:\xampp\htdocs\laravel_demo_app\resources\views/auth/registration.blade.php ENDPATH**/ ?>
